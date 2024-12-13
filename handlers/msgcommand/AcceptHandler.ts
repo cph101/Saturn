3b22fff0d02e3dc6b3ea744d8103831abe153c86
cph101 <cph101@icloud.com>
2024-12-13 12:22:27 +0000

    Off you go babyy

--- handlers/msgcommand/AcceptHandler.ts
+++ handlers/msgcommand/AcceptHandler.ts
@@ -26,14 +26,14 @@ export class AcceptHandler extends EventHandler<"messageCreate"> {
     async handle(message: OmitPartialGroupDMChannel<Message<boolean>>) {
         await SuperUsers.refreshUsers();
         const users = SuperUsers.getUsers();
-        
-    
+
+
         const author: EmbedAuthorOptions = {
             name: message.author.username,
             iconURL: message.author.avatarURL()
         };
-    
-    
+
+
         let embed: EmbedBuilder = new EmbedBuilder()
             .setColor(0xFFFFFF)
             .setAuthor(author);
@@ -41,45 +41,44 @@ export class AcceptHandler extends EventHandler<"messageCreate"> {
         if (!Object.keys(users).includes(message.author.id)) {
             embed.setColor(0xED4245);
             embed.setTitle("Error: you do not have access to admin commands");
-    
+
             await message.reply({ embeds: [embed] });
             return;
         }
-    
+
         const args = message.content.match(/^,(accept|ax) (<@!?(\d{17,20})>|\d{17,20}) (.*)$/);
-        console.log(args)
         const member = args[3] ?? args[2];
         const guild = args[4];
-    
+
         if (!guild) return;
-    
+
         embed.setColor(0xFFD700)
             .setDescription("Accepting, please wait...");
         const waitingMessage = await message.reply({ embeds: [embed] });
-    
+
         if (Clans.lastUpdated <= (Date.now() - (1000 * 60 * 90))) {
             await Clans.refreshCache(null);
         }
-    
+
         const guildData = Clans.getClanMemberCounts().map(({ clan }) => clan).find(candidate => {
             if (candidate.name.toLowerCase() == guild.toLowerCase()) return true;
             return guild.toLowerCase() == "long" && candidate.id == "966137238880682145";
         });
-    
+
         if (!guildData) {
-                embed.setColor(0xED4245)
-                embed.setDescription(`No such guild, \`${guild}\``);
+            embed.setColor(0xED4245)
+            embed.setDescription(`No such guild, \`${guild}\``);
             await waitingMessage.edit({ embeds: [embed] });
             return;
         }
-        
+
         AcceptHandler.commandRan = Date.now();
         const application = await this.fetchApplication(guildData, message, "", member);
-    
+
+        const guildInfo: ClanDetails = await ClanDetails.get(guildData.id);
+
         if (application) {
             await this.tryAccept(application["id"], application["guild_id"]);
-    
-            const guildInfo: ClanDetails = await ClanDetails.get(guildData.id);
             embed.setColor(0xFFFFFF)
             if (guildData.name.toLowerCase() == "hail") {
                 embed.setDescription(`<@${member}> (\`${member}\`) has been accepted into <:HAIL:1311970450112249896>**\`#${guildData.name}\`**`);
@@ -91,41 +90,48 @@ export class AcceptHandler extends EventHandler<"messageCreate"> {
                 });
             }
             const guild = await SaturnBot.INSTANCE.client.guilds.fetch("1244682239187619940")
-            const channel = await guild.channels.fetch("1299078683008565349"); 
+            const channel = await guild.channels.fetch("1299078683008565349");
             if (channel.isSendable()) {
                 channel.send(embed.toJSON().description)
             } else {
                 throw new AssertionError()
             }
         } else {
-            embed.setColor(0xED4245)
-                .setDescription(`No application was found for <@${member}> (\`${member}\`) in \`${guildData.name}\` or the bot timed out.`);
-            await waitingMessage.edit({ embeds: [embed] });
+            if (guildData.name.toLowerCase() == "hail") {
+                embed.setDescription(`No application was found for <@${member}> (\`${member}\`) in <:HAIL:1311970450112249896>**\`#${guildData.name}\`** or the bot timed out.`);
+                await waitingMessage.edit({ embeds: [embed] });
+            } else {
+                await guildInfo.withIconImage(async (emoji) => {
+                    embed.setColor(0xED4245)
+                        .setDescription(`No application was found for <@${member}> (\`${member}\`) in <:${emoji.name}:${emoji.id}>**\`#${guildData.name}\`** or the bot timed out.`);
+                    await waitingMessage.edit({ embeds: [embed] });
+                });
+            }
         }
 
-    }    
+    }
 
     async fetchApplication(guildData: Clan, interaction: OmitPartialGroupDMChannel<Message<boolean>>, before: string, userID: string) {
         if (Date.now() - AcceptHandler.commandRan >= 20000) return null;
 
         const response = await axios.get(
-        `https://discord.com/api/v9/guilds/${guildData.id}/requests?status=SUBMITTED${before != "" ? "&before=" + before : ""}`,
-        {
-            headers: {
-                Authorization: SaturnBot.UB_TOKEN,
+            `https://discord.com/api/v9/guilds/${guildData.id}/requests?status=SUBMITTED${before != "" ? "&before=" + before : ""}`,
+            {
+                headers: {
+                    Authorization: SaturnBot.UB_TOKEN,
+                }
+            }
+        )
+
+        const joinReqs: Object[] = response.data["guild_join_requests"]
+        const maybe = joinReqs.find(obj => obj["user"]["id"] == userID)
+
+        if (!maybe) {
+            if (joinReqs.reverse()[0] && joinReqs.reverse()[0]["id"]) {
+                return await this.fetchApplication(guildData, interaction, joinReqs.reverse()[0]["id"], userID)
             }
         }
-    )
-
-    const joinReqs: Object[] = response.data["guild_join_requests"]
-    const maybe = joinReqs.find(obj => obj["user"]["id"] == userID)
-
-    if (!maybe) {
-        if (joinReqs.reverse()[0] && joinReqs.reverse()[0]["id"]) {
-            return await this.fetchApplication(guildData, interaction, joinReqs.reverse()[0]["id"], userID)
-        }
-    }
-    return maybe;
+        return maybe;
     }
 
     async tryAccept(joinReqID: string, guildID: string) {
@@ -149,5 +155,5 @@ export class AcceptHandler extends EventHandler<"messageCreate"> {
     async canHandle(message: OmitPartialGroupDMChannel<Message<boolean>>) {
         return message.content.match(/^,(accept|ax) (<@!?(\d{17,20})>|\d{17,20}) .*$/) != null;
     }
-    
+
 }
